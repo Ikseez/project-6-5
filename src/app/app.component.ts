f699272082505065d5079f8442ebb63eac91f937
Ikseez <calalen32@gmail.com>
2018-06-05 15:13:56 -0700

    practice-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from './classes/person';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+  heading = 'Person Details';
+  person: any
+  public myPeeps: Person = new Person();
+  result: any[];
+
+  constructor() {
+    console.log(this.myPeeps.peoples);
+    this.result = this.myPeeps.peoples;
+  }
+} 
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//  selector: 'app-root',
+//  templateUrl: './app.component.html',
+//  styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//  title = 'app';
+//  heading = 'Person Details';
+//  person: any;
+// }
+
+// class person {
+//  name: {
+//    firstName: 'none',
+//    lastName: 'none',
+//  }
+//  gender: '???'
+// }
---
+++ src/app/classes/person.ts
@@ -0,0 +1,16 @@
+export class Person {
+    public peoples:any[] = [{
+        gender: 'Mr',
+        firstName : 'John', 
+        lastName : 'Doe',
+        age : 25
+    },{
+        gender: 'Ms',
+        firstName : 'Emily',
+        lastName : 'Smith',
+        age : 'ded',
+        city: 'NYC'
+    }]
+    public doThings() {}
+}
+
